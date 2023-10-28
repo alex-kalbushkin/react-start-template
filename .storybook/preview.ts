e19788d7ca81027b07c98279e6ea49fcf01ba6e4
akolbushkin <akolbushkin@tula.co>
2023-10-28 19:00:27 +0300

    storybook localization in progress

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,12 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import React, { useEffect } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../src/providers/LocalizationProvider/settings';
+import { LocaleLanguage } from '../src/providers/LocalizationProvider/types';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -13,3 +17,38 @@ const preview: Preview = {
 };
 
 export default preview;
+
+// Wrap stories in the I18nextProvider component
+const withI18next = (Story, context) => {
+  const { locale } = context.globals;
+
+  // When the locale global changes
+  // Set the new locale in i18n
+  useEffect(() => {
+    i18n.changeLanguage(locale);
+  }, [locale]);
+
+  return React.createElement(
+    React.Suspense,
+    { fallback: React.createElement('div', null, 'loading translations...') },
+    React.createElement(I18nextProvider, { i18n: i18n }, React.createElement(Story))
+  );
+};
+
+// export decorators for storybook to wrap stories in
+export const decorators = [withI18next];
+
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: LocaleLanguage.En, title: 'English' },
+        { value: LocaleLanguage.Ru, title: 'Русский' },
+      ],
+      showName: true,
+    },
+  },
+};
